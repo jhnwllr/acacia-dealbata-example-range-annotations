54b9d681734791c893e65d88d069e823eaa255d9
John Waller <ftw712@ku.dk>
2023-04-04 11:37:08 +0200

    first commit

---
+++ R/annotation_req.sh
@@ -0,0 +1,30 @@
+
+http://labs.gbif.org:7013/
+
+# curl -u "jwaller:#9SkdnAksiDkneksQosnVid88A" -X POST http://labs.gbif.org:7013/v1/occurrence/annotation/project -H "Content-Type: application/json" -d '{"name":"John Examples", "description":"John example annotations"}'
+
+
+# curl -X 'POST' \
+  'http://labs.gbif.org:7013/v1/occurrence/annotation/rule' \
+  -H 'accept: */*' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  # "id": 0,
+  # "contextType": "TAXON",
+  # "contextKey": "string",
+  # "geometry": "string",
+  # "errorType": "IDENTIFICATION",
+  # "enrichmentType": "INTRODUCED",
+  # "projectId": 0,
+  # "supportedBy": [
+    # "string"
+  # ],
+  # "contestedBy": [
+    # "string"
+  # ],
+  # "created": "2023-03-28T09:33:22.060Z",
+  # "createdBy": "string",
+  # "deleted": "2023-03-28T09:33:22.060Z",
+  # "deletedBy": "string"
+# }'
+
